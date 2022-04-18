a4f5efe7aa7ad72b4bbb90d82369ae9278022b29
imran18026 <mdimran.ossain.ist@gmail.com>
2022-04-19 01:11:17 +0600

    create product details page

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,44 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
-import Head from 'next/head';
 import React from 'react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from '@material-ui/core';
 import useStyles from '../utils/styles';
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Next Amazona</title>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-        <Typography>All Right reserved. Next Amazona, 2022</Typography>
+        <Typography>All rights reserved. Next Amazona.</Typography>
       </footer>
     </div>
   );
